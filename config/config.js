c81eebed128e859ad9459ae83a04fe0dceff25e4
ayor <dosumuayomide@gmail.com>
2021-05-15 16:16:59 +0100

    careerBliss scrapper

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT:  process.env.PORT || 5000,
     INDEED_URI: process.env.INDEED_URI, 
+    CAREER_BLISS_URI: process.env.CAREER_BLISS_URI, 
 }
--- scrapper/careerBliss/careerBliss-scrapper.js
+++ scrapper/careerBliss/careerBliss-scrapper.js
@@ -1,13 +1,13 @@
-const indeed_uri = require('../../config/config').INDEED_URI;
+const CAREER_BLISS_URI = require('../../config/config').CAREER_BLISS_URI;
 
-const __url = indeed_uri;
 
 
 module.exports = {
     INDEED_SCRAPPER: async (browser, company_name) => {
 
         try {
-            let review_link = `https://www.indeed.com/cmp/${company_name}/reviews`
+            
+            let review_link = `https://www.careerbliss.com/search/?q=${company_name}&l=&typeFilter=review&sf=true`;
             let page = await browser.newPage();
             //set user agent to prevent the site from treating this scrapper as a bot;
             const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
