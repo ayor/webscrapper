aa75b692b8e41c182df8f4b8a0799b330856a4c8
ayor <dosumuayomide@gmail.com>
2021-06-10 19:24:27 +0100

    set timer for memory cache

--- util/setClient.js
+++ util/setClient.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 let client = redis.createClient(process.env.REDIS_URL, { no_ready_check: true });
 
 const setClient = (companyName, data) => {
-    client.set(companyName, JSON.stringify(data)); 
+    client.setex(companyName,3600, JSON.stringify(data)); 
 }
 
 module.exports = setClient;
