8f21e5ea1c5c6b19882b8cec8cda78f1e9796953
ayor <dosumuayomide@gmail.com>
2021-06-10 18:44:21 +0100

    switched to api

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/scrapper-api/v1/comments",// "https://mlw-api.herokuapp.com/scrapper-api/v1/comments",
+    baseURL:"https://mlw-api.herokuapp.com/scrapper-api/v1/comments",
     headers: {
         'Content-Type': 'application/json',
     }
 })
 
-//"http://localhost:5000/scrapper-api/v1/comments",
+//"http://localhost:5000/scrapper-api/v1/comments",x
