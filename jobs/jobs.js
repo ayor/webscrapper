5ca9d30f8a677ccc0fe19a958ff42a18fb78facf
Onyedikachi Shedrach Erugo <36213804+Onyedikachi123@users.noreply.github.com>
2021-06-12 16:12:31 +0100

    Update jobs.js

--- jobs/jobs.js
+++ jobs/jobs.js
@@ -9,49 +9,31 @@ const scrapeProcess = async ({ data }) => {
         let { company_name, _badPageId, _goodPageId } = data;
         let browser = await puppeteerBrowser();
         let { reviews, numberReviews } = await pageScraper.scrapper.glassdoor_scrapper(browser, company_name);
-//         let response = await pageScraper.scrapper.indeed_scrapper( browser, company_name, false );
 
-        let { goodComments, badComments } = analyzeReviews([...reviews], numberReviews); //, ...response.reviews
+        let response = await pageScraper.scrapper.indeed_scrapper( browser, company_name, false );
+        let gld_reviews = numberReviews.toString().split("K");
+        let ind_reviews = response.numberReviews.toString().split("K");
+        numberReviews = +gld_reviews[0] + +ind_reviews[0];
+        let { goodComments, badComments } = analyzeReviews([...reviews, ...response.reviews], numberReviews); 
+
 
         let goodPercent = ((goodComments.length / (goodComments.length + badComments.length)) * 100).toFixed(2) || 0;
         let badPercent = (100 - goodPercent).toFixed(2) || 0;
 
-        client.get(company_name, (err, data) => {
-            let __totalReviews;
-            if (err) {
-                throw new Error(err);
-            }
-            data = JSON.parse(data);
-            /**
-             * add the total number of reviews
-             * concat the good and bad reviews
-             */
-            let ind_reviews = numberReviews.toString().split("K");
-            let old_reviews = data.numberReviews.toString().split("K");
-
-
-            if (ind_reviews.length > 1 || old_reviews.length > 1) {
-                __totalReviews = +ind_reviews[0] + +old_reviews[0] + "K";
-            } else {
-                __totalReviews = +ind_reviews[0] + +old_reviews[0]
-            }
-
-            let _updatedBadCommments = [...data.comments.badComments, ...badComments];
-            let _updatedGoodCommments = [...data.comments.goodComments, ...goodComments];
-
-
-            client.setex(company_name,3600, JSON.stringify({
-                company_name,
-                comments: { badComments: _updatedBadCommments, goodComments: _updatedGoodCommments },
-                goodPercent,
-                goodPageId: _goodPageId,
-                badPageId: _badPageId,
-                badPercent,
-                reviewStatus: "ACT",
-                numberReviews: __totalReviews || 0,
-            }));
-        })
-
+        if(gld_reviews.length > 1 || ind_reviews.length > 1){
+            numberReviews += "K" 
+        }
+        
+        client.setex(company_name,3600, JSON.stringify({
+            company_name,
+            goodPercent,
+            goodPageId: _goodPageId,
+            badPageId: _badPageId,
+            badPercent,
+            comments: { badComments, goodComments },
+            reviewStatus: "ACT",
+            numberReviews: numberReviews || 0,
+        }));
         // console.log(job.id);
 
     } catch (error) {
@@ -63,3 +45,31 @@ const scrapeProcess = async ({ data }) => {
 
 
 module.exports = scrapeProcess;
+
+
+// client.get(company_name, (err, data) => {
+        //     let __totalReviews;
+        //     if (err) {
+        //         throw new Error(err);
+        //     }
+        //     data = JSON.parse(data);
+        //     /**
+        //      * add the total number of reviews
+        //      * concat the good and bad reviews
+        //      */
+        //     let ind_reviews = numberReviews.toString().split("K");
+        //     let old_reviews = data.numberReviews.toString().split("K");
+
+
+        //     if (ind_reviews.length > 1 || old_reviews.length > 1) {
+        //         __totalReviews = +ind_reviews[0] + +old_reviews[0] + "K";
+        //     } else {
+        //         __totalReviews = +ind_reviews[0] + +old_reviews[0]
+        //     }
+
+        //     let _updatedBadCommments = [...data.comments.badComments, ...badComments];
+        //     let _updatedGoodCommments = [...data.comments.goodComments, ...goodComments];
+
+
+           
+        // })
