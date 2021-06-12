a5c607fb2dfe810310aaf0c40c50601f0b4eac8c
ayor <dosumuayomide@gmail.com>
2021-06-12 22:44:29 +0100

    removed env variable

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -37,7 +37,7 @@ const Search = props => {
                 <div className="col text-center ">
                     <form onSubmit={handleSearchBtn}>
                         <div className={"form-group my-3 mx-auto " + SearchClass.FormGroup}>
-                            <span className={"fa fa-search " + SearchClass.FormIcon}></span>
+                            <span className={"fa fa-search input-group-prepend " + SearchClass.FormIcon}></span>
                             <input onChange={handleChange} type="text" className={"form-control " + SearchClass.SearchInput} placeholder="Search Your Company Name..." />
                             <p className="text-danger">{errorMessage}</p>
                         </div>
