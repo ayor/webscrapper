106b83188636d39bf7ae149e0c23b4bd46c47292
ayor <dosumuayomide@gmail.com>
2021-06-13 08:15:46 +0100

    adding logs to the jobs

--- jobs/jobs.js
+++ jobs/jobs.js
@@ -6,6 +6,7 @@ const analyzeReviews = require('../util/analyzeReviews');
 
 const scrapeProcess = async ({ data }) => {
     try {
+        console.log("started-job")
         let { company_name, _badPageId, _goodPageId } = data;
         let browser = await puppeteerBrowser();
         let { reviews, numberReviews } = await pageScraper.scrapper.glassdoor_scrapper(browser, company_name);
@@ -14,16 +15,19 @@ const scrapeProcess = async ({ data }) => {
         let gld_reviews = numberReviews.toString().split("K");
         let ind_reviews = response.numberReviews.toString().split("K");
         numberReviews = +gld_reviews[0] + +ind_reviews[0];
+        console.log("analyzing-reviews")
         let { goodComments, badComments } = analyzeReviews([...reviews, ...response.reviews], numberReviews); 
-
-
+        console.log("analyzing-reviews-finshed")
+        console.log("calculating-percentage")
         let goodPercent = ((goodComments.length / (goodComments.length + badComments.length)) * 100).toFixed(2) || 0;
         let badPercent = (100 - goodPercent).toFixed(2) || 0;
+        console.log("calculated-percentage")
 
         if(gld_reviews.length > 1 || ind_reviews.length > 1){
             numberReviews += "K" 
         }
-        
+        console.log("storing in redis")
+
         client.setex(company_name,3600, JSON.stringify({
             company_name,
             goodPercent,
@@ -35,6 +39,7 @@ const scrapeProcess = async ({ data }) => {
             numberReviews: numberReviews || 0,
         }));
         // console.log(job.id);
+        console.log("completed job"); 
 
     } catch (error) {
         console.log(error)
