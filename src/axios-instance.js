87bef6eb11893278c22cf69cb6e3d87886298c98
Onyedikachi Shedrach Erugo <36213804+Onyedikachi123@users.noreply.github.com>
2021-06-14 10:51:14 +0100

    Update axios-instance.js

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,//process.env.BASE_URL_PROD,// "http://localhost:5000/scrapper-api/v1/comments",
+    baseURL: "https://mlw-api.herokuapp.com/scrapper-api/v1/comments",//process.env.BASE_URL,//process.env.BASE_URL_PROD,// "http://localhost:5000/scrapper-api/v1/comments",
     headers: {
         'Content-Type': 'application/json',
     }
