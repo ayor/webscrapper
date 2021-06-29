4a475c20fd0b1eda60b32e1df6d76435152989a7
ayor <dosumuayomide@gmail.com>
2021-06-29 11:42:17 +0100

    pagination update

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -4,12 +4,13 @@ const Pagination = props => {
     return (
         <nav aria-label="Page navigation">
             <ul className="pagination justify-content-center mt-3">
-                {props.pageId  > 1 ?  <li className="page-item">
-                    <button className={ props.prevIsDisabled ? `page-link disbaled` : "page-link"} onClick={props.handlePrevBtn} tabIndex="-1">Previous</button>
+                {props.pageId > 1 ? <li className="page-item">
+                    <button className={props.prevIsDisabled ? `page-link disbaled` : "page-link"} onClick={props.handlePrevBtn} tabIndex="-1">Previous</button>
                 </li> : null}
-               
+
                 <li className="page-item">
-                    <button className={ props.nextIsDisabled ? `page-link disbaled` : "page-link"} onClick={props.handleNextBtn} href="#">Next</button>
+                    { props.length < 10 ? null : <button className={props.nextIsDisabled ? `page-link disbaled` : "page-link"} onClick={props.handleNextBtn} href="#">Next</button>
+                    }
                 </li>
             </ul>
         </nav>
